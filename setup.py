a577c19e1296df413100f6858ded39ab71e757a8
Pete Gadomski <pete.gadomski@gmail.com>
2015-07-10 08:52:55 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="sbd",
+      version="0.1",
+      description="Iridium Short Burst Data DirectIP handling",
+      author="Pete Gadomski",
+      author_email="pete.gadomski@gmail.com",
+      url="https://github.com/gadomski/sbd",
+      )
