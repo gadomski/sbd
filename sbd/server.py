03486af5b0d5d7d097d00fd4ac36681e0b139424
Pete Gadomski <pete.gadomski@gmail.com>
2015-07-10 08:58:16 -0600

    Use handle, not finish
    I'm pretty sure (not totally, but pretty) that we're guaranteed to get a
    whole complete request in `handle`.

--- sbd/server.py
+++ sbd/server.py
@@ -5,5 +5,5 @@ from sbd import MobileOriginatedMessage
 
 class IridiumTcpHandler(SocketServer.StreamRequestHandler):
 
-    def finish(self):
+    def handle(self):
         print MobileOriginatedMessage.parse(self.rfile).data
