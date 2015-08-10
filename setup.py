d0163cb608b299733ad47111f0ef6b982d1a9a1c
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-10 11:54:00 -0600

    Add sbd package to install

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name="sbd",
       author="Pete Gadomski",
       author_email="pete.gadomski@gmail.com",
       url="https://github.com/gadomski/sbd",
+      packages=["sbd"],
       scripts=["bin/iridiumd"],
       install_requires=[
           "python-daemon",
